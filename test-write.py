d367d1f8758dfb11d180155e9157af6c202fa066
nutnote <lq772507659@gmail.com>
2016-05-09 11:11:36 +0800

    add test-write.py and dns.txt

---
+++ test-write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+ 
+with open('./dns.txt', 'a+') as fo:
+    fo.seek(0)
+    fo.readline()
+    cur = fo.tell() - 2
+    fo.seek(cur)
+    fo.write('8.8.8.8')
+    fo.seek(0)
+    fo.readlines()
